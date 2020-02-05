ff34035b552103d85c8e8998c8c244e50306b9d7
nilesh dubey <nileshdubey@haartyhanks.com>
2020-02-05 19:25:47 +0530

    lesson 4-8 - Handling get request

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const env_port = process.env.PORT || 3000;
 const express = require("express");
 const expressApp = express();
 
+let courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+  { id: 4, name: "course4" }
+];
+
 // let result = _.contains([1, 2, 3], 2);
 
 // console.log(result);
@@ -22,6 +29,15 @@ expressApp.get("/blog/:type/:id", (req, res) => {
   res.send(
     `This is for Query ${query.query} \r\n This is  param ${param.type}   ${param.id}`
   );
+});
+
+expressApp.get("/course/:id", (req, res) => {
+  let course = courses.find(c => {
+    return c.id === parseInt(req.params.id);
+  });
+
+  if (!course) res.send("400 page - Not found");
+  res.send(course);
 });
 
 expressApp.listen(env_port, () => console.log("localhost:" + env_port));
